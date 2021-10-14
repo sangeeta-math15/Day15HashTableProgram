6dee22ffc0e665e201197ad4de75ac3aaa915373
sngtmth <sngtmth@gmail.com>
2021-10-15 00:16:21 +0530

    Added: Remove a word avoidable from the phrase

--- src/test/java/com/bridgelabz/MyHashMapTest.java
+++ src/test/java/com/bridgelabz/MyHashMapTest.java
@@ -48,4 +48,25 @@ public class MyHashMapTest {
         int frequency=myHashMapParanoid.get("paranoid");
         Assertions.assertEquals(3,frequency);
     }
+    @Test
+    public void givenASentence_whenRemovedAvoidableWord_shouldRemoveAvoidableWord(){
+        String sentence = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately " +
+                "into paranoid avoidable situations";
+        MyHashMapParanoid<String, Integer> myLinkedHashMap = new MyHashMapParanoid<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for(String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value += 1;
+            myLinkedHashMap.add(word, value);
+        }
+        System.out.println("before:" + myLinkedHashMap);
+        myLinkedHashMap.remove("avoidable");
+        System.out.println("after: " + myLinkedHashMap);
+        Integer removedWordFrequency = myLinkedHashMap.get("avoidable");
+        Assertions.assertEquals(null,removedWordFrequency);
+    }
 }
